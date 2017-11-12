16122f06cb4b693437a138fcd877610a9dcfe180
Joel Park <jgpark@indiana.edu>
2017-11-12 14:54:55 -0500

    Finished saving formula

--- fe/src/components/pages/CreateLeague/Index.js
+++ fe/src/components/pages/CreateLeague/Index.js
@@ -1,39 +1,57 @@
-import React from 'react';
-import { fromJS, List, Set } from 'immutable';
-import { connect } from 'react-redux';
+import React from "react";
+import { fromJS, List, Set } from "immutable";
+import { connect } from "react-redux";
 
-import API from '../../../middleware/API';
+import API from "../../../middleware/API";
 
-import Btn from '../../common/Btn/Btn';
-import LabelInput from '../../common/Input/LabelInput';
-import Spinner from '../../common/Spinner/Spinner';
+import Btn from "../../common/Btn/Btn";
+import LabelInput from "../../common/Input/LabelInput";
+import Spinner from "../../common/Spinner/Spinner";
 
-import classnames from 'classnames/bind';
-import style from './Index.css';
+import classnames from "classnames/bind";
+import style from "./Index.css";
 const cx = classnames.bind(style);
 
 class CreateLeague extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'My League',
-      limit: '2',
+      name: "My League",
+      limit: "2"
     };
   }
 
-
-  updateLeagueTime = () => {
-
-  }
+  updateLeagueTime = () => {};
   createLeague = ({ name, limit }) => {
     this.setState({
-      loading: true,
+      loading: true
     });
 
-    var d1 = Math.round(new Date().getTime()/1000.0); // pass date from draft page
+    var d1 = Math.round(new Date().getTime() / 1000.0); // pass date from draft page
     const mutation = `
         mutation{
-          CreateLeague(data: {name: "${name}", limit: ${limit}, epoc_date: ${d1} }){
+          CreateLeague(data: {name: "${name}", limit: ${limit}, epoc_date: ${d1}, formula: {tdpass : 4, 
+                                                                                            passyds: 25,
+                                                                                            tdrush: 6,
+                                                                                            rushyds: 10,
+                                                                                            recyds: 10,
+                                                                                            passyds: 5,
+                                                                                            fgmade: 3,
+                                                                                            fgmissed: -1,
+                                                                                            xpmade: 1,
+                                                                                            intthrow: -2,
+                                                                                            int: 2,
+                                                                                            fumlost: -2,
+                                                                                            sack: 1,
+                                                                                            forcedfum: 2,
+                                                                                            kickblock: 2,
+                                                                                            puntblock: 2,
+                                                                                            tdkickret: 6,
+                                                                                            saf: 2,
+                                                                                            tdpuntret: 6,
+                                                                                            tddef: 6,
+                                                                                            i20punt: 1,
+                                                                                            puntyds: 50,}}){
             _id
           }
         }
@@ -41,11 +59,11 @@ class CreateLeague extends React.PureComponent {
 
     API.GraphQL(mutation).then(res => {
       if (res.data.CreateLeague._id) {
-        window.location.href = '#/app/league/list';
+        window.location.href = "#/app/league/list";
       } else {
         window.alert(res);
         this.setState({
-          loading: false,
+          loading: false
         });
       }
     });
@@ -62,14 +80,14 @@ class CreateLeague extends React.PureComponent {
   handleCreateLeague = () => {
     const { name, limit } = this.state;
 
-    if (!name || name === '') {
-      window.alert('Please give name');
+    if (!name || name === "") {
+      window.alert("Please give a league name");
       return;
     }
 
     const limitNum = parseInt(limit, 10);
     if (isNaN(limitNum) || limitNum < 2 || limitNum > 10) {
-      window.alert('Max Players must be a number between 2 and 10');
+      window.alert("Max Players must be a number between 2 and 10");
       return;
     }
     this.createLeague({ name, limit: limitNum });
@@ -79,26 +97,26 @@ class CreateLeague extends React.PureComponent {
     const { state, props } = this;
     const { name, limit } = state;
     return (
-      <div className={cx('root')}>
-        <div className={cx('name')}>Create New League</div>
-        <div className={cx('input-wrap')}>
+      <div className={cx("root")}>
+        <div className={cx("name")}>Create New League</div>
+        <div className={cx("input-wrap")}>
           <LabelInput
             label="League Name"
             value={name}
             onChange={this.changeName}
           />
         </div>
-        <div className={cx('input-wrap')}>
+        <div className={cx("input-wrap")}>
           <LabelInput
             label="League Max Players"
             value={limit}
             onChange={this.changeLimit}
           />
         </div>
-        <div className={cx('input-wrap')}>
-          <LabelInput label="Formular Param 1" disabled />
+        <div className={cx("input-wrap")}>
+          <LabelInput label="Formula Param 1" disabled />
         </div>
-        <div className={cx('function-bar')} onClick={this.handleCreateLeague}>
+        <div className={cx("function-bar")} onClick={this.handleCreateLeague}>
           <Btn>Create</Btn>
         </div>
       </div>
@@ -108,6 +126,6 @@ class CreateLeague extends React.PureComponent {
 
 export default connect(stores => {
   return {
-    accountStore: stores.account,
+    accountStore: stores.account
   };
 })(CreateLeague);
