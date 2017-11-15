8c9bc3576d2462cbbef45fafae0caac6284012f9
Joel Park <jgpark@indiana.edu>
2017-11-15 11:58:52 -0500

    Changed updatestats to add current week's stats to total stats.

--- fe/src/components/pages/Login/Index.js
+++ fe/src/components/pages/Login/Index.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import Spinner from '../../common/Spinner/Spinner';
-import LabelInput from '../../common/Input/LabelInput';
-import Btn from '../../common/Btn/Btn';
+import Spinner from "../../common/Spinner/Spinner";
+import LabelInput from "../../common/Input/LabelInput";
+import Btn from "../../common/Btn/Btn";
 
-import API from '../../../middleware/API';
-import { OAUTH_URL } from '../../../config';
+import API from "../../../middleware/API";
+import { OAUTH_URL } from "../../../config";
 
-import classnames from 'classnames/bind';
-import style from './Index.css';
+import classnames from "classnames/bind";
+import style from "./Index.css";
 const cx = classnames.bind(style);
 
 class Login extends React.PureComponent {
@@ -18,8 +18,8 @@ class Login extends React.PureComponent {
     super(props);
     this.state = {
       loading: false,
-      username: '',
-      password: '',
+      username: "",
+      password: ""
     };
   }
 
@@ -37,28 +37,28 @@ class Login extends React.PureComponent {
 
     const param = {
       username,
-      password,
+      password
     };
 
     this.setState({
-      loading: true,
+      loading: true
     });
 
     API.API((request, endpoint) => {
       return request
         .post(`${endpoint}/login`)
-        .type('form')
+        .type("form")
         .send(param);
     }).then(res => {
       this.setState({
-        loading: false,
+        loading: false
       });
       if (res.err === 0) {
-        window.location.href = '#/app/news';
+        window.location.href = "#/app/news";
       } else if (res.err === 2) {
-        window.location.href = '#' + res.duoUrl;
+        window.location.href = "#" + res.duoUrl;
       } else {
-        alert('Passowrd is not correct, or account not existed');
+        alert("Passowrd is not correct, or account does not exist");
       }
     });
   };
@@ -71,16 +71,16 @@ class Login extends React.PureComponent {
     const { state, props } = this;
     const { loading, username, password } = state;
     return (
-      <div className={cx('root')}>
-        <div className={cx('bg', 'abs-center')}>
-          <div className={cx('cover')} />
+      <div className={cx("root")}>
+        <div className={cx("bg", "abs-center")}>
+          <div className={cx("cover")} />
         </div>
-        <div className={cx('content', 'abs-center')}>
-          <div className={cx('main-title')}>Your Game, Your Team</div>
-          <div className={cx('title')}>Sing in to your NFL account</div>
-          <form className={cx('box')}>
-            <Spinner className={cx('loader')} show={loading} />
-            <div className={cx('form')}>
+        <div className={cx("content", "abs-center")}>
+          <div className={cx("main-title")}>Your Game, Your Team</div>
+          <div className={cx("title")}>Sign in to your NFL account</div>
+          <form className={cx("box")}>
+            <Spinner className={cx("loader")} show={loading} />
+            <div className={cx("form")}>
               <LabelInput
                 label="Email"
                 type="text"
@@ -94,12 +94,12 @@ class Login extends React.PureComponent {
                 onChange={this.changePassword}
               />
             </div>
-            <div className={cx('functio-bar')}>
-              <Link className={cx('link')} to="/create_account">
+            <div className={cx("functio-bar")}>
+              <Link className={cx("link")} to="/create_account">
                 Create account
               </Link>
-              <Link className={cx('link')} to="/forget_password">
-                Forget password?
+              <Link className={cx("link")} to="/forget_password">
+                Forgot password?
               </Link>
             </div>
 
@@ -107,7 +107,7 @@ class Login extends React.PureComponent {
               SIGN IN
             </Btn>
             <Btn
-              className={cx('oauth-btn')}
+              className={cx("oauth-btn")}
               type="primary"
               onClick={this.oauth}
             >
--- fe/src/components/pages/News/Index.js
+++ fe/src/components/pages/News/Index.js
@@ -1,40 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import classnames from 'classnames/bind';
-import style from './Index.css';
+import classnames from "classnames/bind";
+import style from "./Index.css";
 const cx = classnames.bind(style);
 
 class News extends React.PureComponent {
   render() {
     const { props } = this;
     return (
-      <div className={cx('root')}>
-        <div className={cx('main-title')}>Recent News:</div>
-        <div className={cx('item')}>
-          <span className={cx('date')}>10/20</span>
-          <span className={cx('text')}>Add League List</span>
+      <div className={cx("root")}>
+        <div className={cx("main-title")}>Recent News:</div>
+        <div className={cx("item")}>
+          <span className={cx("date")}>10/20</span>
+          <span className={cx("text")}>Add League List</span>
         </div>
-        <div className={cx('item')}>
-          <span className={cx('date')}>10/18</span>
-          <span className={cx('text')}>Add Google OAuth Login</span>
+        <div className={cx("item")}>
+          <span className={cx("date")}>10/18</span>
+          <span className={cx("text")}>Add Google OAuth Login</span>
         </div>
-        <div className={cx('item')}>
-          <span className={cx('date')}>10/14</span>
-          <span className={cx('text')}>
+        <div className={cx("item")}>
+          <span className={cx("date")}>10/14</span>
+          <span className={cx("text")}>
             Player List Page Add Search/Filter Function
           </span>
         </div>
-        <div className={cx('item')}>
-          <span className={cx('date')}>10/4</span>
-          <span className={cx('text')}>Add Player List Page</span>
+        <div className={cx("item")}>
+          <span className={cx("date")}>10/4</span>
+          <span className={cx("text")}>Add Player List Page</span>
         </div>
-        <div className={cx('item')}>
-          <span className={cx('date')}>9/24</span>
-          <span className={cx('text')}>Add Login Functionality</span>
+        <div className={cx("item")}>
+          <span className={cx("date")}>9/24</span>
+          <span className={cx("text")}>Add Login Functionality</span>
         </div>
-        <div className={cx('item')}>
-          <span className={cx('date')}>9/30</span>
-          <span className={cx('text')}>Add News Page</span>
+        <div className={cx("item")}>
+          <span className={cx("date")}>9/30</span>
+          <span className={cx("text")}>Add News Page</span>
         </div>
       </div>
     );
