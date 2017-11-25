0b3d12bf9a525602ce457d8643a88854fc72cbd2
mofas223 <mofas223@gmail.com>
2017-11-24 20:38:48 -0500

    better player detail

--- fe/src/components/pages/PlayerDetail/Index.js
+++ fe/src/components/pages/PlayerDetail/Index.js
@@ -59,49 +59,62 @@ class PlayerDetail extends React.PureComponent {
 
     return (
       <div className={cx('root')}>
-        <Link to={`/app/league/list`} style={{ display: 'inline-block' }}>
+        <Link to={`/app/player/list`} style={{ display: 'inline-block' }}>
           <BackBtn type="secondary">Back to List</BackBtn>
         </Link>
 
-        <div className={cx('main-title')}>Player Info</div>
+        <div className={cx('info-row')}>
+          <div className={cx('basic-info', 'block')}>
+            <div
+              className={cx('bg-img')}
+              style={{
+                width: '100%',
+                height: '100%',
+                opacity: 0.1,
+                background: `url(http://i.nflcdn.com/static/site/7.5/img/logos/165x185/${player.get(
+                  'Team'
+                )}.png) no-repeat 50% 50%`,
+              }}
+            >
+              <div className={cx('bg-img-cover')} />
+            </div>
 
-        <div className={cx('basic-info', 'block')}>
-          <div className={cx('thumb')} />
-          <div className={cx('info')}>
-            <div className={cx('name')}>
-              <div className={cx('thick')}>Name </div> {player.get('Name')}{' '}
+            <div className={cx('thumb')} />
+            <div className={cx('info')}>
+              <div className={cx('info-item', 'name')}>
+                <div className={cx('bold')}>Name</div>
+                {player.get('Name')}
+              </div>
+              <div className={cx('info-item', 'position')}>
+                <div className={cx('bold')}>Position</div>
+                {player.get('Position')}
+              </div>
+              <div className={cx('info-item', 'team')}>
+                <div className={cx('bold')}>Team</div>
+                {player.get('Team')}
+              </div>
+              <div className={cx('info-item', 'rank')}>
+                <div className={cx('bold')}>Fantasy Rank</div>
+                {player.get('Rank')}
+              </div>
             </div>
-            <div className={cx('position')}>
-              <div className={cx('thick')}>Position </div>{' '}
-              {player.get('Position')}
-            </div>
-            <div className={cx('team')}>
-              <div className={cx('thick')}>Team </div>
-              {player.get('Team')}{' '}
-            </div>
-            <div className={cx('rank')}>
-              <div className={cx('thick')}>Fantasy Rank</div>{' '}
-              {player.get('Rank')}{' '}
-            </div>
+          </div>
+
+          <div className={cx('block', 'video')}>
+            {player.get('URL') ? (
+              <iframe
+                src={'https://www.youtube.com/embed/'.concat(
+                  player.get('URL').split('=')[1]
+                )}
+                width="560"
+                height="315"
+                frameBorder="0"
+                allowFullScreen
+              />
+            ) : null}
           </div>
         </div>
-        {player.get('URL') ? (
-          <div className={cx('main-title')}>Highlights</div>
-        ) : null}
-        {player.get('URL') ? (
-          <div className={cx('news', 'block')}>
-            <iframe
-              src={'https://www.youtube.com/embed/'.concat(
-                player.get('URL').split('=')[1]
-              )}
-              width="560"
-              height="315"
-              frameBorder="0"
-              allowFullScreen
-            />
-          </div>
-        ) : null}
-        <div className={cx('main-title')}>Ability Record</div>
+
         <div className={cx('ability', 'block')}>
           <div>Passing Yards: {player.get('Passing_Yards')} </div>
           <div>Rushing Yards: {player.get('Rushing_Yards')}</div>
