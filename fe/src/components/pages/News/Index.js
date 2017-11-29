ec17e037bbf3c65e49b0203652c79d100792f50b
mofas223 <mofas223@gmail.com>
2017-11-29 15:16:47 -0500

    put some logo there

--- fe/src/components/pages/News/Index.js
+++ fe/src/components/pages/News/Index.js
@@ -1,40 +1,28 @@
-import React from "react";
+import React from 'react';
 
-import classnames from "classnames/bind";
-import style from "./Index.css";
+import classnames from 'classnames/bind';
+import style from './Index.css';
 const cx = classnames.bind(style);
 
 class News extends React.PureComponent {
   render() {
     const { props } = this;
     return (
-      <div className={cx("root")}>
-        <div className={cx("main-title")}>Recent News:</div>
-        <div className={cx("item")}>
-          <span className={cx("date")}>10/20</span>
-          <span className={cx("text")}>Add League List</span>
-        </div>
-        <div className={cx("item")}>
-          <span className={cx("date")}>10/18</span>
-          <span className={cx("text")}>Add Google OAuth Login</span>
-        </div>
-        <div className={cx("item")}>
-          <span className={cx("date")}>10/14</span>
-          <span className={cx("text")}>
-            Player List Page Add Search/Filter Function
-          </span>
-        </div>
-        <div className={cx("item")}>
-          <span className={cx("date")}>10/4</span>
-          <span className={cx("text")}>Add Player List Page</span>
-        </div>
-        <div className={cx("item")}>
-          <span className={cx("date")}>9/24</span>
-          <span className={cx("text")}>Add Login Functionality</span>
-        </div>
-        <div className={cx("item")}>
-          <span className={cx("date")}>9/30</span>
-          <span className={cx("text")}>Add News Page</span>
+      <div className={cx('root')}>
+        <h1 className={cx('title')}>Team List</h1>
+        <div className={cx('wrap')}>
+          <a href="http://www.azcardinals.com/">
+            <img src="https://static.nfl.com/static/site/img/logos/svg/teams/ARI.svg" />
+          </a>
+          <a href="http://www.atlantafalcons.com/">
+            <img src="https://static.nfl.com/static/site/img/logos/svg/teams/ATL.svg" />
+          </a>
+          <a href="http://www.azcardinals.com/">
+            <img src="https://static.nfl.com/static/site/img/logos/svg/teams/BAL.svg" />
+          </a>
+          <a href="http://www.azcardinals.com/">
+            <img src="https://static.nfl.com/static/site/img/logos/svg/teams/BUF.svg" />
+          </a>
         </div>
       </div>
     );
